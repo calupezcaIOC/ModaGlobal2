06da540fdba2f860d68cda73fa837c9fa9fc3b72
Carolina Lopez Casanova <carorusk@gmail.com>
2025-12-09 22:49:00 +0000

    Inicialització del projecte ModaGlobal amb HTML, CSS i JS

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("Pàgina carregada correctament");
+  const titol = document.querySelector("h1");
+  if (titol) {
+    titol.style.color = green;
+  }
+});
